8d9249838e2ca8b45b866b35447c5fcae0cc741a
Misla Deile Oliveira de Luna Silva <rm90348@estudante.fieb.edu.br>
2025-04-09 21:07:43 -0300

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import UserTable from "./components/UserTable";
-import "./styles/styles.css"; // opcional
+import React from 'react'
+import './styles.css' 
 
 function App() {
   return (
-    <div className="App">
-      <UserTable />
+    <div className="login-container">
+
+      <h1>Bem-vindo ao TaskNavigation 💼</h1>
+      <p>FUNCIONA</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
